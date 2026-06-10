9272ec92c6bb1bfcc8f76db3c2c177b654f011b6
Sathish <sathishkrishnan369@gmail.com>
2026-06-10 21:40:58 +0530

    Structured Response

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+from typing import List
+from pydantic import BaseModel,Field
 import os   
 
 
@@ -10,6 +12,19 @@ from langchain_core.messages import HumanMessage
 from tavily import TavilyClient
 
 tavily_client = TavilyClient()
+
+class Source ( BaseModel):
+    """
+    Schema for a source used by the agent
+    """
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse ( BaseModel):
+    """
+    Schema for the agent's response
+    """
+    answer: str = Field( description="The answer provided by the agent")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used by the agent")
 
 @tool
 def search(query: str) -> str:
@@ -24,12 +39,13 @@ def search(query: str) -> str:
 
 llm = ChatOpenAI()
 tools = [search]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Welcome to the LangSmith Agent Example!")
     result = agent.invoke({"messages": [HumanMessage(content="Give me 2 motivation quotes based on the volleyball sport")]})
     print(result)
+    
     
 if __name__ == "__main__":
     main()
